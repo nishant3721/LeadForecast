821149fa59efb29694e4b0d28356619a8fd65a0f
Rajan <Rajangoyal125@gmail.com>
2021-09-09 20:20:30 +0530

    search functionality added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import React from "react";
 import "./App.css";
 import CurrentWeather from "./Components/CurrentWeather";
 import Navbar from "./Components/Navbar";
-
 function App() {
   return (
     <>
